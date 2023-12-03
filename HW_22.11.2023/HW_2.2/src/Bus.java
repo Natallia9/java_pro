1a5d832b6958fb3f4b6aacef97a82174805e2b40
Natallia9 <140706832+Natallia9@users.noreply.github.com>
2023-12-03 18:08:57 +0100

    Delete HW_22.11.2023/HW_2.2 directory

--- HW_22.11.2023/HW_2.2/src/Bus.java
+++
@@ -1,4 +0,0 @@
-public class Bus extends Vehicle{
-    private boolean accessibility;
-    private int busNumber;
-}
--- HW_22.11.2023/HW_2.2/src/Vehicle.java
+++
@@ -1,8 +0,0 @@
-public class Vehicle {
-    private String model;
-    private int capacity;
-    private double fare;
-    private String route;
-
-
-}
